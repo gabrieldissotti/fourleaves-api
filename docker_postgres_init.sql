486eb65889ee98898290e80775d868aeabb44312
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-11-13 16:00:44 -0300

    feat(docker environment): updated docker config of database

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -1,8 +1,13 @@
+CREATE USER fourleaves WITH PASSWORD 'fourleaves' CREATEDB;
+
 CREATE DATABASE fourleaves
     WITH
-    OWNER = postgres
+    OWNER = fourleaves
     ENCODING = 'UTF8'
     LC_COLLATE = 'en_US.utf8'
     LC_CTYPE = 'en_US.utf8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
+
+
+GRANT ALL PRIVILEGES ON DATABASE fourleaves TO fourleaves;
