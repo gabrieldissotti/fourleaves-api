f92599f344cbf50bd02c51592296ff698f929062
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-11-26 18:53:44 -0300

    chore(deploy): configured environment

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,17 @@
 import { createConnection } from 'typeorm';
 
 /* read ormconfig.json by default */
-createConnection();
+createConnection({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: Number(process.env.DATABASE_PORT) || 5432,
+  username: process.env.DATABASE_USERNAME || 'fourleaves',
+  password: process.env.DATABASE_PASSWORD || 'fourleaves',
+  database: process.env.DATABASE_NAME || 'fourleaves',
+  entities: ['./src/models/*.ts'],
+  migrations: ['./src/database/migrations/*.ts'],
+  logging: process.env.DATABASE_LOGGING === 'true',
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
+});
