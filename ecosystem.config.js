8a3ccb8e123364bd1c92ff75a14c63c5db328630
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-11-26 19:47:12 -0300

    build(pm2): test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,18 @@ module.exports = {
   apps : [{
     script: './dist/src/server.js',
     watch: './dist/src/',
-    cwd: "/var/apis/sorteiu_api/current/dist/src/"
   }],
 
   deploy : {
-    production : {
+    production: {
+      name : 'sorteiu_api',
       user : 'root',
       host : '104.248.13.181',
       ref  : 'origin/master',
       repo : 'git@github.com:gabrieldissotti/fourleaves-api.git',
       path : '/var/apis/sorteiu_api',
       'pre-deploy-local': '',
-      'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production && cp .env ./dist/src/',
+      'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production --cwd /var/apis/sorteiu_api/current/dist/src/ && cp .env ./dist/src/',
       'pre-setup': '',
     }
   }
