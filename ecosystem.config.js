c03115a0927c628ec9a8deb03a84bd7abd29bea4
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-11-27 01:41:59 -0300

    fix(pre deploy): removed arguments

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,6 @@ module.exports = {
       repo : 'git@github.com:gabrieldissotti/fourleaves-api.git',
       path : '/var/apis/sorteiu_api',
       'pre-deploy-local': '',
-      'pre-deploy': 'rm -r ./source/*',
       'post-deploy' : 'yarn && yarn build && cd /var/apis/sorteiu_api/source/dist/src/ && pm2 delete sorteiu_api && cp /var/apis/sorteiu_api/source/.env /var/apis/sorteiu_api/source/dist/src/ && pm2 start server.js --name sorteiu_api',
       'pre-setup': '',
     }
