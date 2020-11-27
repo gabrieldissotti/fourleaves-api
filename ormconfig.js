d02b7e46ab1dd0fddc0c7a88f2713a78a3cbb0d9
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-11-27 01:39:40 -0300

    fix(orm config): fixed problens on production

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,7 @@
 require('dotenv/config')
 
+const extensionFiles = process.env.NODE_ENV === 'development' ? '.ts' : '.js'
+
 module.exports = {
   type: 'postgres',
   host: process.env.DATABASE_HOST || 'localhost',
@@ -8,8 +10,8 @@ module.exports = {
   password: process.env.DATABASE_PASSWORD || 'fourleaves',
   database: process.env.DATABASE_NAME || 'fourleaves',
   logging: process.env.DATABASE_LOGGING === 'true',
-  entities: ["./src/models/*.ts"],
-  migrations: ["./src/database/migrations/*.ts"],
+  entities: [ "./src/models/*"+extensionFiles],
+  migrations: ["./src/database/migrations/*"+extensionFiles],
   logging: true,
   cli: {
     "migrationsDir": "./src/database/migrations"
