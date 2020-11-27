99cbb2ef307ae6a11c89f42a57d9a1c4921d7103
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-11-27 02:03:06 -0300

    fix(ormconfig): fixed definitelly problemns

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,9 @@
 require('dotenv/config')
 
 const extensionFiles = process.env.NODE_ENV === 'development' ? '.ts' : '.js'
-console.log('extensionFiles', extensionFiles)
-console.log('__dirname', __dirname)
+
+const basePath = process.env.NODE_ENV === 'development' ? '.' : __dirname + '/dist';
+
 module.exports = {
   type: 'postgres',
   host: process.env.DATABASE_HOST || 'localhost',
@@ -11,8 +12,8 @@ module.exports = {
   password: process.env.DATABASE_PASSWORD || 'fourleaves',
   database: process.env.DATABASE_NAME || 'fourleaves',
   logging: process.env.DATABASE_LOGGING === 'true',
-  entities: [ __dirname +"/dist/src/models/*"+extensionFiles],
-  migrations: [__dirname +"/dist/src/database/migrations/*"+extensionFiles],
+  entities: [ basePath + "/src/models/*" + extensionFiles],
+  migrations: [basePath + "/src/database/migrations/*" + extensionFiles],
   logging: true,
   cli: {
     "migrationsDir": "./src/database/migrations"
